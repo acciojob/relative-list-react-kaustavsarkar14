e5d5f7a2c4b44dbecb22aeed96d8922bff104479
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-11-26 23:36:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const names = ['a', 'b']
   return (
     <div id="main">
                {/* Do not remove the main div */}
+    <ol>
+    {
+      data.map((el,i)=><li key={"relativeListItem"+(i+1)} >el</li>}
+    }
+    </ol>
     </div>
   )
 }
